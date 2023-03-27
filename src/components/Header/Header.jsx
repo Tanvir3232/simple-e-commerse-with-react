16eb551f1f7662183ebfa97ef156a6e38883a48b
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-03-27 10:13:49 +0600

    added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src={logo} alt="" />
+            <nav>
+                <a href="/order">Order</a>
+                <a href="/order-review">Order Review</a>
+                <a href="/manage-invetory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
